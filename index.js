2b115345b48e491ddecdcd8d11a43c68a888c18c
billymoughton94 <47531935+billymoughton94@users.noreply.github.com>
2019-02-19 12:46:06 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -17,12 +17,16 @@ export default class Iphone extends Component {
 		// temperature state
 		this.state.temp = "";
 		// button display state
-		this.setState({ display: true });
+		this.setState({ display: true, visibleImg : false  });
 	}
+
+//============================================================
+//=============== WEATHER DATA FETCH AND DISPLAY =============
 
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
+		this.setState({visibleImg : true});
 		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=dc5c2808dc2c8fd769f66c7deaae5130";
 		$.ajax({
 			url: url,
@@ -31,21 +35,63 @@ export default class Iphone extends Component {
 			error : function(req, err){ console.log('API call failed ' + err); }
 		})
 		// once the data grabbed, hide the button
-		this.setState({ display: false });
-	}
+		this.setState({ display: false});
 
-	findIcon(conditions) {
+	}
+//============================================================
+//============================================================
+
+//============================================================
+//=============== FORCAST DATA FETCH AND DISPLAY =============
+	fetchForcastData = () => {
+		var url = "http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&APPID=dc5c2808dc2c8fd769f66c7deaae5130";
+		$.ajax({
+			url: url,
+			dataType: "jsonp",
+			success : this.parseForecastResponse,
+			error : function(req, err){ console.log('API call failed ' + err); }
+		})
+
+	}
+//============================================================
+//============================================================
+
+
+
+//==================================================
+//=============== WEATHER ICON FETCH =============
+
+	mainWeatherSymbol(conditionsCode) {
 		// "../../assets/icons/Large_sunny.png"
-		switch (conditions) {
-			case "light intensity shower rain": return '../../assets/icons/Large_rain.png';
-			case "light rain": return '../../assets/icons/Small_rain.png';
-			default: return '../../assets/icons/Large_sunny.png';
+
+		switch (conditionsCode) {
+			case 300: case 301: case 302: case 310: case 311:
+			case 312: case 313: case 314: case 321: return '../../assets/icons/Large_rain.png';
+
+			case 500: case 501: case 502: case 503:
+			case 504: case 511: case 520: case 521:
+			case 522: case 531: return '../../assets/icons/Large_rain.png';
+
+			case 600: case 601: case 602: case 611:
+			case 612: case 615: case 616: case 620:
+			case 621: case 622: return '../../assets/icons/Large_snow.png';
+
+			case 801: case 802: case 803:
+			case 804: return '../../assets/icons/Large_cloudy.png';
+
+			case 800: return  '../../assets/icons/Large_sunny.png';
+
+			default: return;
 		}
 	}
+//==================================================
+//==================================================
 
-	//this.findIcon = this.findIcon.bind(this);
 
 
+
+//==================================
+//=============== MAIN =============
 
 	// the main render method for the iphone component
 	render() {
@@ -57,30 +103,41 @@ export default class Iphone extends Component {
 			<div class={ style.container }>
 				<div class={ style.header }>
 					<div class={ style.city }>{ this.state.locate} {this.state.country }</div>
+					<span class={ tempStyles }> {this.state.visibleImg ? (<img src = {this.mainWeatherSymbol(this.state.code)} />) : null}  { this.state.temp }</span>
 					<div class={ style.conditions }>{ this.state.cond }</div>
-					<img class = {style.weatherSymbol} src = {this.findIcon(this.state.cond)} />
-					<span class={ tempStyles }> { this.state.temp }</span>
+					{this.state.visibleImg ? (<div> <h1> TEST </h1> </div>) : null}
 				</div>
 				<div class={ style.details }></div>
 				<div class= { style_iphone.container }>
-					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData }/ > : null }
+					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData } /> : null }
 				</div>
 			</div>
 		);
 	}
+
+//==================================
+//==================================
+
+
 
 	parseResponse = (parsed_json) => {
 		var location = parsed_json['name'];
 		var countryName = parsed_json['sys']['country'];
 		var temp_c = parseInt(parsed_json['main']['temp']);
 		var conditions = parsed_json['weather']['0']['description'];
+		var condCode = parsed_json['weather']['0']['id'];
 
 		// set states for fields so they could be rendered later on
 		this.setState({
 			locate: location,
 			country: countryName,
 			temp: temp_c,
-			cond : conditions
+			cond : conditions,
+			code : condCode,
 		});
+	}
+
+	parseForecastResponse = (parsed_json) => {
+
 	}
 }
